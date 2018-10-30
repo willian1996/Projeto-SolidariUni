2a5722d967c4dd4c06ba182dccf8797bfa386442
liansz <37086572+liansz@users.noreply.github.com>
2018-10-30 16:15:03 -0200

    dropdown
    voltei as alterações

--- js/script.js
+++ js/script.js
@@ -12,9 +12,9 @@
                   inDuration: 300,
                   outDuration: 225,
                   constrainWidth: false, // Does not change width of dropdown to that of the activator
-                  hover: false, // hover desativado
+                  hover: true, // hover ativado
                   gutter: 0, // Spacing from edge
-                  belowOrigin: true, // Displays dropdown below the button
+                  belowOrigin: false, // Displays dropdown below the button
                   alignment: 'left', // Displays dropdown with edge aligned to the left of button
                   stopPropagation: false // Stops event propagation
                 
