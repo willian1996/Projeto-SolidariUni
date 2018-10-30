9fb1544dd345a0ad579fc523fdce8406ebb5d94f
liansz <37086572+liansz@users.noreply.github.com>
2018-10-30 16:12:50 -0200

    estou testando dropdown

--- js/script.js
+++ js/script.js
@@ -14,7 +14,7 @@
                   constrainWidth: false, // Does not change width of dropdown to that of the activator
                   hover: false, // hover desativado
                   gutter: 0, // Spacing from edge
-                  belowOrigin: false, // Displays dropdown below the button
+                  belowOrigin: true, // Displays dropdown below the button
                   alignment: 'left', // Displays dropdown with edge aligned to the left of button
                   stopPropagation: false // Stops event propagation
                 
