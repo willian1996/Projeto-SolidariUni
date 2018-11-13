0e156b4a20754c809ff0a4d0ac4392003c2998b7
Willian Sales <williansalesgabriel@hotmail.com>
2018-11-13 01:35:29 -0200

    Update js/script.js

--- js/script.js
+++ js/script.js
@@ -1,144 +1,144 @@
-    $( document ).ready(function(){
-            //slidenav
-            $(".button-collapse").sideNav();
-            // Slideshow
-            $('.slider').slider();
-            $('.parallax').parallax();
-            //modal trigger
-            $('.modal').modal();
-            $('select').material_select();
-            //dropdown menu usuario
-            $('.dropdown-button').dropdown({
-                  inDuration: 300,
-                  outDuration: 225,
-                  constrainWidth: false, // Does not change width of dropdown to that of the activator
-                  hover: true, // hover ativado
-                  gutter: 0, // Spacing from edge
-                  belowOrigin: false, // Displays dropdown below the button
-                  alignment: 'left', // Displays dropdown with edge aligned to the left of button
-                  stopPropagation: false // Stops event propagation
-                
-            });
-            //abrir dropdown menu usuario 
-            $('.dropdown-button').dropdown('open');
-            //fechar dropdown menu usuario 
-            $('.dropdown-button').dropdown('close');
-            // Initialize collapse button
-            $(".button-collapse").sideNav();
-        
-        //PLUGIN DO LUAN (JQUERYMASK)//
-        
-            $("#telefone").mask("(12)00000-0000");
-            $("#CPF").mask("000.000.000-00");
-            $("#CNPJ").mask("00.000.000/0000-00");
-        
-        $('#password6').keyup(function() {
-            $('#result').html(checkStrength($('#password6').val()))
-            })
-        function checkStrength(password) {
-            var strength = 0
-            if (password.length < 6) {
+$(document).ready(function(){
+    //slidenav
+    $(".button-collapse").sideNav();
+    // Slideshow
+    $('.slider').slider();
+    $('.parallax').parallax();
+    //modal trigger
+    $('.modal').modal();
+    $('select').material_select();
+    //dropdown menu usuario
+    $('.dropdown-button').dropdown({
+          inDuration: 300,
+          outDuration: 225,
+          constrainWidth: false, // Does not change width of dropdown to that of the activator
+          hover: true, // hover ativado
+          gutter: 0, // Spacing from edge
+          belowOrigin: false, // Displays dropdown below the button
+          alignment: 'left', // Displays dropdown with edge aligned to the left of button
+          stopPropagation: false // Stops event propagation
+
+    });
+    //abrir dropdown menu usuario 
+    $('.dropdown-button').dropdown('open');
+    //fechar dropdown menu usuario 
+    $('.dropdown-button').dropdown('close');
+    // Initialize collapse button
+    $(".button-collapse").sideNav();
+
+    //PLUGIN DO LUAN (JQUERYMASK)//
+
+    $("#telefone").mask("(12)00000-0000");
+    $("#CPF").mask("000.000.000-00");
+    $("#CNPJ").mask("00.000.000/0000-00");
+
+    $('#password6').keyup(function() {
+        $('#result').html(checkStrength($('#password6').val()))
+        })
+    function checkStrength(password) {
+        var strength = 0
+        if (password.length < 6) {
+        $('#result').removeClass()
+        $('#result').addClass('curta')
+        return 'Senha muito curta!'
+        }
+        if (password.length > 7) strength += 1
+
+        if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) strength += 1
+
+
+        if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/)) strength += 1
+
+
+        if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1
+
+
+        if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1
+
+
+        if (strength < 2) {
             $('#result').removeClass()
-            $('#result').addClass('curta')
-            return 'Senha muito curta!'
-            }
-                if (password.length > 7) strength += 1
+            $('#result').addClass('fraca')
+            return 'Senha fraca!'
+        } else if (strength == 2) {
+            $('#result').removeClass()
+            $('#result').addClass('boa')
+            return 'Senha boa!'
+        } else {
+            $('#result').removeClass()
+            $('#result').addClass('forte')
+            return 'Senha forte!'
+        }
+    }
 
-                if (password.match(/([a-z].*[A-Z])|([A-Z].*[a-z])/)) strength += 1
+});
 
+function optionCheck(){
+    var option = document.getElementById("options").value;
+    if(option == "pessoa-fisica"){
+        $('#hiddenFisica').fadeIn(); 
+        $('#hiddenJuridica').fadeOut();   
+    }
+    else if(option == "pessoa-juridica"){
+        $('#hiddenFisica').fadeOut(); 
+        $('#hiddenJuridica').fadeIn(); 
 
-                if (password.match(/([a-zA-Z])/) && password.match(/([0-9])/)) strength += 1
+    }
+}
 
+function login(){
+    var email = $('#login_email').val();
+    var senha = $('#login_senha').val();
 
-                if (password.match(/([!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1
+    console.log("Email: "+email+" senha: "+senha);
 
-
-                if (password.match(/(.*[!,%,&,@,#,$,^,*,?,_,~].*[!,%,&,@,#,$,^,*,?,_,~])/)) strength += 1
-
-            
-            if (strength < 2) {
-                $('#result').removeClass()
-                $('#result').addClass('fraca')
-                return 'Senha fraca!'
-            } else if (strength == 2) {
-                $('#result').removeClass()
-                $('#result').addClass('boa')
-                return 'Senha boa!'
-            } else {
-                $('#result').removeClass()
-                $('#result').addClass('forte')
-                return 'Senha forte!'
-            }
-            }
-        
-        });
-
-        function optionCheck(){
-            var option = document.getElementById("options").value;
-            if(option == "pessoa-fisica"){
-                $('#hiddenFisica').fadeIn(); 
-                $('#hiddenJuridica').fadeOut();   
-            }
-            else if(option == "pessoa-juridica"){
-                $('#hiddenFisica').fadeOut(); 
-                $('#hiddenJuridica').fadeIn(); 
-                
+    $.ajax({
+        url: "server/login.php",
+        method: "POST",
+        dataType: "json",
+        data: {'email': email, 'senha': senha},
+        success: function(retorno){
+            if(retorno.deucerto){
+                location.reload();
+            }else{
+                alert(retorno.mensagem);
             }
         }
+    });
+}
+function cadastrarCampanha(){
+    var formData = new FormData();
+    var titulo = $('#titulo').val();
+    var subtitulo = $('#subtitulo').val();
+    var descricao = $('#descricao').val();
+    var data_final_campanha = $('#data_final_campanha').val();
+    var foto_campanha = $('#foto_campanha').prop('files')[0];
 
-        function login(){
-            var email = $('#login_email').val();
-            var senha = $('#login_senha').val();
+    formData.append("titulo", titulo);
+    formData.append("subtitulo", subtitulo);
+    formData.append("descricao", descricao);
+    formData.append("data_final_campanha", data_final_campanha);
+    formData.append("foto", foto_campanha);
 
-            console.log("Email: "+email+" senha: "+senha);
+    $('#titulo, #subtitulo, #descricao, #data_final_campanha, #foto_campanha').val('');
 
-            $.ajax({
-                url: "server/login.php",
-                method: "POST",
-                dataType: "json",
-                data: {'email': email, 'senha': senha},
-                success: function(retorno){
-                    if(retorno.deucerto){
-                        location.reload();
-                    }else{
-                        alert(retorno.mensagem);
-                    }
-                }
-            });
-        }
-        function cadastrarCampanha(){
-                    var formData = new FormData();
-                    var titulo = $('#titulo').val();
-                    var subtitulo = $('#subtitulo').val();
-                    var descricao = $('#descricao').val();
-                    var data_final_campanha = $('#data_final_campanha').val();
-                    var foto_campanha = $('#foto_campanha').prop('files')[0];
-
-                    formData.append("titulo", titulo);
-                    formData.append("subtitulo", subtitulo);
-                    formData.append("descricao", descricao);
-                    formData.append("data_final_campanha", data_final_campanha);
-                    formData.append("foto", foto_campanha);
-                    
-                    $('#titulo, #subtitulo, #descricao, #data_final_campanha, #foto_campanha').val('');
-
-                    $.ajax({
-                          url: "server/adicionar-campanha-backend.php",
-                          method: "POST",
-                          dataType: "json",
-                          data: formData,
-                          cache: false,
-                          contentType: false,
-                          processData: false,
-                          success: function(retorno){
-                              if(retorno.deucerto){
-                                    alert(retorno.msg);     
-                              }else{
-                                    alert(retorno.msg);
-                              }
-                          }
-                      });
-        }
+    $.ajax({
+          url: "server/adicionar-campanha-backend.php",
+          method: "POST",
+          dataType: "json",
+          data: formData,
+          cache: false,
+          contentType: false,
+          processData: false,
+          success: function(retorno){
+              if(retorno.deucerto){
+                    alert(retorno.msg);     
+              }else{
+                    alert(retorno.msg);
+              }
+          }
+    });
+}
         
 
         
