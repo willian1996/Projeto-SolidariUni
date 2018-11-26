ecdd3fec5ff2daa1de34543a4a543fd7f6adf0c9
LuanDePaula <36863013+LuanDePaula@users.noreply.github.com>
2018-11-26 15:54:08 -0200

    sobrenosa
    sdasdasd

--- index.php
+++ index.php
@@ -105,7 +105,7 @@ else{
 <!--------------Conteúdo Textos e frases---------------------->
         <section class="conteudoTexto">
             <div>
-                <h1>Sobre Nós</h1>
+                <h1>A SolidariUni</h1>
                 <p>A SolidariUni é uma associação fundada em meados de 2018, por um grupo de amigos. Buscamos fazer a união entre colaboradores e recebedores de doações. Nosso principal objetivo é unir institutos que precisam de itens específicos e encontrar pessoas ou empresas que possam cooperar de alguma maneira de maneira efetiva e sem fins lucrativos. Participe você também!</p>
         
             </div>
