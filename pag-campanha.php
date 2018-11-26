370ae402d59c97542f52f481ed67304cb0f1909f
Willian Sales <williansalesgabriel@hotmail.com>
2018-11-26 11:30:24 -0200

    Tentando fazer o plugin contagem regressiva

--- pag-campanha.php
+++ pag-campanha.php
@@ -10,134 +10,7 @@ else{
 }
 ?>
 
-<style>
-#perfil-campanha{
-    width: 80%;
-    margin: auto;
-    min-height: 500px;
-}
 
-#perfil-campanha .perfilcamp{
-    background-color: #f2f2f2;
-    width: 80%;
-    min-height: 500px;
-    margin: auto;
-    margin-top: 150px;
-    margin-bottom: 15px;
-    padding: 20px 0;
-}
-
-#perfil-campanha .perfilcamp .fotocamp{
-    width: 200px;
-    height: 200px;
-    background-color: dodgerblue;
-    border-radius: 50%;
-    position: absolute;
-    top: 150px;
-    margin-left: 15px;
-}
-
-#perfil-campanha .perfilcamp .fotocamp img{
-    width: 100%;
-    background-image:url(../img/willian.jpg);
-    border-radius: 100%;
-}
-
-#perfil-campanha .perfilcamp h3{
-    font-size: 38px;
-    margin-left: 20%;
-}
-
-    #perfil-campanha .perfilcamp h2{
-        font-size:30px;
-    }
-
-@media screen and (max-width: 1200px){
-    #perfil-campanha .perfilcamp{
-        background-color: #f2f2f2;
-        width: 80%;
-        margin: auto;
-        margin-top: 150px;
-        margin-bottom: 150px;
-}
-    
-    #perfil-campanha .perfilcamp h3{
-        text-align: center;
-        margin-top: 50%;
-        margin-right: 25%;
-        
-    }
-    
-    #perfil-campanha p{
-        text-align: justify;
-        margin-left: 30px;
-    }
-    
-    #perfil-campanha .perfilcamp h5{
-    font-size: 28px;
-        text-align: center;
-}
-     #perfil-campanha .perfilcamp .emailcont{
-        padding-right: 50px;
-    }
-    
-    #perfil-campanha .perfilcamp p{
-    margin-left: 100px 100px;
-    padding-right: 10%;
-    text-align: justify;
-    }
-        
-    #perfil-campanha .perfilcamp h3{
-        text-align: center;
-        margin: 20px 20px;
-        margin-right: 100px;
-        margin-top: 50%;
-}
-
-    #perfil-campanha .perfilcamp h2{
-    margin-left: 27%;
-}
-
-    #perfil-campanha .perfilcamp h5{
-    text-align: center;
-    font-size: 28px;
-}
-
-    #perfil-campanha .perfilcamp p{
-    margin-left: 27%;
-    padding-right: 50px;
-    text-align: justify;
-    }
-}
-    
-    #perfil-campanha .perfilcamp h3{
-    margin-left: 27%;
-}
-
-    #perfil-campanha .perfilcamp h2{
-    margin-left: 27%;
-}
-
-    #perfil-campanha .perfilcamp h5{
-    margin-left: 27%;
-    font-size: 28px;
-}
-
-#perfil-campanha .perfilcamp p{
-    margin-left: 27%;
-    padding-right: 50px;
-    text-align: justify;
-}
-.camp-comentarios{
-    background-color: #f2f2f2;
-    width: 80%;
-    min-height: 500px;
-    margin: auto;
-    margin-bottom: 150px;
-    padding: 20px 40px ;
-}
-
-</style>
 <section id="perfil-campanha">
     <?php
     $id = isset($_GET['id']) && is_numeric($_GET['id']) ? $_GET['id'] : 0;
@@ -161,6 +34,30 @@ else{
         <h3><?php echo $valor['titulo']; ?></h3>
             <div class="dados-campanha">
                 <h5><?php echo $valor['subtitulo']; ?></h5><p><?php echo $valor['descricao']; ?></p>
+                <br>
+                
+                
+                
+                
+                <h5>Encerra em:</h5>
+                 
+                
+    <div class="wrapper">
+      <div class="cell">
+        <div id="holder">
+          <div class="digits"></div>
+        </div>
+      </div>
+    </div>
+                           
+                
+                
+                
+
+                
+                
+                
+                <br>
                 <h2>Contatos:</h2>
                 <p>Contactar: <?php echo $valor['titular']; ?></p>
                 <p><?php echo $valor['telefone_titular']; ?></p>
